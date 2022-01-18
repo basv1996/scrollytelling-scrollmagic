1da69855f9f2ef672d212ecd1242f50d4a87af9c
basv1996 <bas.vugts@gmail.com>
2022-01-18 13:14:10 +0100

    animatie voor de secties 3 en 4

--- scripts/script.js
+++ scripts/script.js
@@ -69,7 +69,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let timeline2 = new TimelineMax()
     timeline2.to('.top .image-container', 4, {
         height: 0
-        //in de css staat de height op 100% en hier verander ik de hoogte naar 0%
+        // in de css staat de height op 100% en hier verander ik de hoogte naar 0%
     })
 
     let Scene2 = new ScrollMagic.Scene({
@@ -81,4 +81,69 @@ document.addEventListener('DOMContentLoaded', () => {
     .setTween(timeline2) // voeg de timeline variabele toe als 
     .setPin('.second-section') // ga niet voorbij de eerste sectie voordat de animatie is geweest
     .addTo(controller)
+
+    // Section 3
+
+       let timeline3 = new TimelineMax()
+       timeline3.to('.section_3_01', 4, {
+           y: -250,
+           ease: Power3.easeInOut
+       })
+       .to('.section_3_02', 4, {
+        y: -200,
+        ease: Power3.easeInOut
+        }, '-=4')
+        .to('.section_3_03', 4, {
+        y: -100,
+        ease: Power3.easeInOut
+        }, '-=4')
+        .to('.section_3_04', 4, {
+        y: 0,
+        ease: Power3.easeInOut
+        }, '-=4')
+        .to('.section_3_05', 4, {
+        y: 150,
+        ease: Power3.easeInOut
+        }, '-=4')
+        .to('.section_3_06', 4, {
+        y: 250,
+        ease: Power3.easeInOut
+        }, '-=4')
+   
+       let Scene3 = new ScrollMagic.Scene({
+           triggerElement: '.third-section',
+           duration: '100%',
+           triggerHook: 0, // als de eerste sectie de bovenkant raakt van het scherm begin dan met animeren
+           offset: '0' // de animatie start als de eerste sectie 200px van de top van het scherm af zit
+       })
+       .setTween(timeline3) // voeg de timeline variabele toe als 
+       .setPin('.third-section') // ga niet voorbij de eerste sectie voordat de animatie is geweest
+       .addTo(controller)
+
+       // section 4
+
+    let timeline4 = new TimelineMax()
+    timeline4.to('.section_4_01', 4, {
+        autoAlpha: 0
+    })
+    .from('.section_4_02', 4, {
+        autoAlpha: 0
+    }, '-=4')
+    .from('.section_4_03', 4, {
+        autoAlpha: 0
+    })
+    .from('.section_4_04', 4, {
+        autoAlpha: 0
+    })
+
+    let Scene4 = new ScrollMagic.Scene({
+        triggerElement: '.forth-section',
+        duration: '100%',
+        triggerHook: 0, // als de eerste sectie de bovenkant raakt van het scherm begin dan met animeren
+        offset: '100' // de animatie start als de eerste sectie 100px van de top van het scherm af zit
+    })
+    .setTween(timeline4) // voeg de timeline variabele toe als 
+    .setPin('.forth-section') // ga niet voorbij de eerste sectie voordat de animatie is geweest
+    .addTo(controller)
+
 })
